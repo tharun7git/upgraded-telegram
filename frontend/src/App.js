00ba2d851cf31470b7f8cccf7cb98304c89c91bf
Tarun <116146974+tharun7git@users.noreply.github.com>
2025-06-16 11:24:53 +0530

    Update App.js
    Signed-off-by: Tarun <116146974+tharun7git@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,11 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import React from 'react';
+import ChangeLogs from './components/ChangeLogs';
 
 function App() {
-  const [data, setData] = useState('');
-
-  useEffect(() => {
-    axios.get('http://4.224.145.208:5000/api/data')
-      .then(response => setData(response.data.message))
-      .catch(error => console.error("Error fetching data:", error));
-  }, []);
-
   return (
     <div>
-      <h1>React + Flask App</h1>
-      <p>{data}</p>
-      <p>tthis is second commit </p>
+      <h1>Welcome to My App</h1>
+      <ChangeLogs />
     </div>
   );
 }
